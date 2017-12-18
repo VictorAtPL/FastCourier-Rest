57b3fdeb0dbbcfe1c9d482b7b9d9f16297989ea8
g0rillaKing <pruchniewski.m@gmail.com>
2017-12-18 18:21:56 +0100

    UC: Zgłoś ofertę - Dodanie komentarzy do kodu.

--- src/main/java/com/magicalsolutions/fastcourier/Entity/ZgloszenieOferty.java
+++ src/main/java/com/magicalsolutions/fastcourier/Entity/ZgloszenieOferty.java
@@ -10,54 +10,99 @@ import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Table(name = "zgloszenia_oferty")
+
+/**
+ * Zdefiniowe klasy ZgloszenieOferty
+ * @author Michał Pruchniewski
+ * @since 0.0.3
+ * @copyright Magical Solutions
+ * @licence Creative Commons Attribution NonCommercial (CC-BY-NC)
+ */
 public class ZgloszenieOferty {
 
+    /**
+     * Atrybut odpowiedzialny za identyfikator zgloszenia
+     */
     @Id
     @Column
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
+    /**
+     * Obiekt ktory jest odpowiedzialny za zgloszonaOferte
+     */
     @ManyToOne(fetch=EAGER)
     @JoinColumn(name="oferta_id")
     @RestResource(path = "zgloszonaOferta", rel="oferta")
     private Oferta zgloszonaOferta;
 
+    /**
+     * Atrybut odpowiedzialny za powód zgloszenia
+     */
     @Column(name="powod")
     @NotEmpty
     private String powod;
 
+    /**
+     * Atrybut odpowiedzialny za treść zgłoszenia
+     */
     @Column(name="tresc")
     @NotEmpty
     private String tresc;
 
+
+    /**
+     * Metoda zwracająca id zgloszenia
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Metoda ustawiająca id zgloszenia
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Metoda pobierajaca zgloszona oferte
+     */
     public Oferta getZgloszonaOferta() {
         return zgloszonaOferta;
     }
 
+    /**
+     * Metoda ustawiajaca zgloszona oferte
+     */
     public void setZgloszonaOferta(Oferta zgloszonaOferta) {
         this.zgloszonaOferta = zgloszonaOferta;
     }
 
+    /**
+     * Metoda pobierajaca powod zgloszenia
+     */
     public String getPowod() {
         return powod;
     }
 
+    /**
+     * Metoda ustawiajaca powod zgloszenia
+     */
     public void setPowod(String powod) {
         this.powod = powod;
     }
 
+    /**
+     * Metoda pobierajaca tresc zgloszenia
+     */
     public String getTresc() {
         return tresc;
     }
 
+    /**
+     * Metoda ustawiajaca tresc zgloszenia
+     */
     public void setTresc(String tresc) {
         this.tresc = tresc;
     }
--- src/main/java/com/magicalsolutions/fastcourier/Repository/ZgloszenieOfertyRepository.java
+++ src/main/java/com/magicalsolutions/fastcourier/Repository/ZgloszenieOfertyRepository.java
@@ -6,5 +6,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "zgloszeniaofert", excerptProjection = VerboseZgloszenieOfertyProjection.class)
+/**
+ * Zdefiniowanie repozytorium modelu ZgloszeniaOferty w endpointcie REST
+ * @author Michał Pruchniewski
+ * @since 0.0.3
+ * @copyright Magical Solutions
+ * @licence Creative Commons Attribution NonCommercial (CC-BY-NC)
+ */
 public interface ZgloszenieOfertyRepository extends CrudRepository<ZgloszenieOferty, Long> {
 }
