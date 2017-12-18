f5c702698a812ad016c46551d978ba1a56e804cb
SirMich <murzyn66@gmail.com>
2017-12-19 00:12:54 +0100

    "UC: Zgłoś uwagę - Dodawanie komentarza do kodu.

--- src/main/java/com/magicalsolutions/fastcourier/Repository/UwagaRepository.java
+++ src/main/java/com/magicalsolutions/fastcourier/Repository/UwagaRepository.java
@@ -4,6 +4,13 @@ import com.magicalsolutions.fastcourier.Entity.Uwaga;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+/**
+ * Interfejs Uwagi
+ * @author Michał Świerczewski
+ * @since 0.0.3
+ * @copyright Magical Solutions
+ * @licence Creative Commons Attribution NonCommercial (CC-BY-NC)
+ */
 @RepositoryRestResource(path = "uwagi")
 public interface UwagaRepository extends CrudRepository<Uwaga, Long> {
 }
