bdf22f2c6ab9955454b4583f072a3025628c6de6
VictorAtPL <p.podbielski@gmail.com>
2018-01-05 14:24:57 +0100

    Konfiguracja CORS aby działał także lokalnie.

--- src/main/java/com/magicalsolutions/fastcourier/Config/CorsConfig.java
+++ src/main/java/com/magicalsolutions/fastcourier/Config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.magicalsolutions.fastcourier.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,8 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 public class CorsConfig extends WebMvcConfigurerAdapter {
 
+    @Value("${spring.profiles.active}")
+    private String active_profile;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        if (active_profile.equals("prod")) {
+            registry.addMapping("/**");
+        }
     }
 }
